89f02d93c10a0b855927e0d86b9a58e32bb80ba9
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:41:27 +0530

    Create Find First Non-Repeating Character in a String.js

---
+++ java_sc/Find First Non-Repeating Character in a String.js
@@ -0,0 +1,21 @@
+let str = "programming";
+let firstUnique = null;
+
+for (let i = 0; i < str.length; i++) {
+    let count = 0;
+    for (let j = 0; j < str.length; j++) {
+        if (str[i] === str[j]) {
+            count++;
+        }
+    }
+    if (count === 1) { // Found first unique character
+        firstUnique = str[i];
+        break;
+    }
+}
+
+if (firstUnique !== null) {
+    console.log("First non-repeating character:", firstUnique);
+} else {
+    console.log("No unique character found");
+}
