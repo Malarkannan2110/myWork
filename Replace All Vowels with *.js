e2148d6ce030de8b384858bcb381e004377e356a
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:49:10 +0530

    Create Replace All Vowels with *.js

---
+++ Replace All Vowels with *.js
@@ -0,0 +1,13 @@
+let str2 = "JavaScript is awesome";
+let vowels = "aeiouAEIOU";
+let replaced = "";
+
+for (let i = 0; i < str2.length; i++) {
+    if (vowels.indexOf(str2[i]) !== -1) {
+        replaced += "*";
+    } else {
+        replaced += str2[i];
+    }
+}
+
+console.log("After replacing vowels:", replaced);
