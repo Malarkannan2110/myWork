2708f914b3aaa311d87c2402db7ec3014c1a6c8b
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:56:25 +0530

    Update Replace All Vowels with *.js

--- java_sc/Replace All Vowels with *.js
+++ java_sc/Replace All Vowels with *.js
@@ -1,1 +1,14 @@
+let str2 = "JavaScript is awesome";
+let vowels = "aeiouAEIOU";
+let replaced = "";
+
+for (let i = 0; i < str2.length; i++) {
+    if (vowels.indexOf(str2[i]) !== -1) {
+        replaced += "*";
+    } else {
+        replaced += str2[i];
+    }
+}
+
+console.log("After replacing vowels:", replaced);
 
