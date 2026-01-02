88562d1060e397c954aaff3771231d25250b291b
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:24:18 +0530

    Create Check Positive, Negative, or Zero.js

---
+++ java_sc/Check Positive, Negative, or Zero.js
@@ -0,0 +1,9 @@
+let num6 = -5;
+
+if (num6 > 0) {
+    console.log(num6, "is Positive");
+} else if (num6 < 0) {
+    console.log(num6, "is Negative");
+} else {
+    console.log(num6, "is Zero");
+}
