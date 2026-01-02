1c6d56b25115405b0bcd81573f6e2719c466df03
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 13:47:36 +0530

    Update reverseString.js

--- basic_javascript/reverseString.js
+++ basic_javascript/reverseString.js
@@ -1,3 +1,17 @@
+
+/**
+ * 📄 reverseString.js
+ * -------------------
+ * This function reverses a string with input validation.
+ * - Checks if input is a string
+ * - Uses split(), reverse(), join()
+ * - Throws error for invalid input
+ *
+ * Example:
+ * console.log(reverseString("JavaScript")); // tpircSavaJ
+ */
+
+
 // Function to reverse a string
 function reverseString(str) {
     if (typeof str !== 'string') {
