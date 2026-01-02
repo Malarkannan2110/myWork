6e28dfccf6239ac65b858551a774cdc4ab286ddc
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:32:20 +0530

    Create Calculate Average of Array Elements.js

---
+++ java_sc/Calculate Average of Array Elements.js
@@ -0,0 +1,9 @@
+let arr14 = [10, 20, 30, 40, 50];
+let sumArr = 0;
+
+for (let i = 0; i < arr14.length; i++) {
+    sumArr += arr14[i];
+}
+
+let average = sumArr / arr14.length;
+console.log("Average of array elements:", average);
