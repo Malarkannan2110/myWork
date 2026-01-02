e90aea92389e46a50f5696bf6ed1ad10a60a4f3b
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:28:15 +0530

    Create Merge Two Arrays (No concat()).js

---
+++ java_sc/Merge Two Arrays (No concat()).js
@@ -0,0 +1,12 @@
+let arr8 = [1, 2, 3];
+let arr9 = [4, 5, 6];
+let mergedArr = [];
+
+for (let i = 0; i < arr8.length; i++) {
+    mergedArr.push(arr8[i]);
+}
+for (let i = 0; i < arr9.length; i++) {
+    mergedArr.push(arr9[i]);
+}
+
+console.log("Merged array:", mergedArr);
