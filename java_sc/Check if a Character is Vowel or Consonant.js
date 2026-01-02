309d895ef2c6832580adc3fd1651c94f37a410f6
Malarkannan2110 <malarkannan2110@gmail.com>
2026-01-02 14:27:23 +0530

    Create Check if a Character is Vowel or Consonant.js

---
+++ java_sc/Check if a Character is Vowel or Consonant.js
@@ -0,0 +1,8 @@
+  let char = "e";
+let vowelsList = "aeiouAEIOU";
+
+if (vowelsList.indexOf(char) !== -1) {
+    console.log(char, "is a vowel");
+} else {
+    console.log(char, "is a consonant");
+}
